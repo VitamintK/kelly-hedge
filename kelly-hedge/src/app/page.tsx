5c72af7a41bbb96a81311b393ac01822a7e7ad6a
Kevin Wang <kevinwang@kevinwang.us>
2023-06-07 12:03:54 -0400

    MVP and even some styling

--- kelly-hedge/src/app/page.tsx
+++ kelly-hedge/src/app/page.tsx
@@ -22,38 +22,113 @@ ChartJS.register(
   Legend
 );
 
+ChartJS.defaults.font.size=18;
+ChartJS.defaults.font.weight="bold";
+ChartJS.defaults.font.family="Helvetica Neue";
+
+const get_hedge_amount = (n: number, w: number, b: number, q: number) => {
+  return (n * (-1 + b + q) + (-1 + b + q - b*q) * w)/(-1 + q);
+}
+const get_hedge_amount_plus = (n: number, w: number, b: number, q: number) => {
+  // return max of hedge amount and 0:
+  return Math.max(get_hedge_amount(n,w,b,q), 0);
+}
+
+type InputContainerProps = {
+  children: React.ReactNode;
+}
+const InputContainer = (props: InputContainerProps) => {
+  return (<div className="">
+    {props.children}
+  </div>);
+}
+
+type InputProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+const Input = (props: InputProps) => {
+  return (<input
+  className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-24 text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-2 ring-1 ring-slate-200 shadow-sm"
+  {...props}
+  >{props.children}</input>);
+
+}
 
 export default function Home() {
   const [answer, setAnswer] = React.useState<number>(0);
+  const [data, setData] = React.useState<{ [x: string]: number }>({
+    potentialWin: 0,
+    netWorth: 0,
+    trueOdds: 0,
+    hedgeOdds: 0,
+  });
+  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget
+    console.log(name, value);
+    const valueNum = Number(value);
+    setData(prevState => ({ ...prevState, [name]: valueNum }));
+  }
   const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     console.log(e);
-    const n = 10000; // net worth
-    const w = 1000; // original potential win
-    const b = 0.4; // current true odds
-    const q = 0.64; // best odds to hedge
-    const ans = (n * (-1 + b + q) + (-1 + b + q - b*q) * w)/(-1 + q);
-    setAnswer(ans);
+    const {potentialWin, netWorth, trueOdds, hedgeOdds} = data;
+    // const n = 10000; // net worth
+    // const w = 1000; // original potential win
+    // const b = 0.4; // current true odds
+    // const q = 0.64; // best odds to hedge
+    // const ans = (netWorth * (-1 + trueOdds + hedgeOdds) + (-1 + trueOdds + hedgeOdds - trueOdds*hedgeOdds) * potentialWin)/(-1 + hedgeOdds);
+    setAnswer(get_hedge_amount(netWorth, potentialWin, trueOdds, hedgeOdds));
   };
+  const XVALS = [100, 200, 400, 800, 1000, 2000, 4000, 6000, 7000, 8000, 10000, 20000, 30000, 40000, 50000, 64000, 100000, 200000];
+  const yvals = XVALS.map(nw => {
+    const {potentialWin, netWorth, trueOdds, hedgeOdds} = data;
+    return get_hedge_amount_plus(nw, potentialWin, trueOdds, hedgeOdds);
+  });
+  const chartData = XVALS.map((x, i) => ({x:x, y:yvals[i]}));
+  console.log(chartData);
+
   return (
-    <div>
-      <div>
-        Potential Win: <input/>
-        Net worth: <input />
-        True odds now: <input />
+    <div className='p-8'>
+      <div className="flex gap-4">
+        <InputContainer>Potential Win: <Input type="number" name="potentialWin" value={data.potentialWin} onChange={handleInputChange} /></InputContainer>
+        <InputContainer>Net worth: <Input type="number" name="netWorth" value={data.netWorth} onChange={handleInputChange} /></InputContainer>
+        <InputContainer>True odds now: <Input type="number" name="trueOdds" value={data.trueOdds} onChange={handleInputChange} /></InputContainer>
+        <InputContainer>Best hedge odds: <Input type="number" name="hedgeOdds" value={data.hedgeOdds} onChange={handleInputChange} /></InputContainer>
         <button onClick={onClick}>Go</button>
       </div>
       <div>
         {answer}
-        <Line data={{
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          datasets: [{
-            label: 'My First Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1
-          }]
-        }} />
+        <Line
+          data={{
+            // labels: XVALS,
+            datasets: [{
+              label: 'My First Dataset',
+              data: chartData,
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              tension: 0.1
+            }],
+          }}
+          options={{
+            title: {
+              display: true,
+              text: 'Hedge Amount vs Starting Net Worth'
+            },
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: 'Starting Net Worth'
+                },
+                type: 'linear',
+                grace: '5%'
+              },
+              y: {
+                title: {
+                  display: true,
+                  text: 'Hedge Amount',
+                }
+              }
+            },
+          }}
+        />
 
 
       </div>
